a216e73e4851feb3913a38ad4df7d52e0c68ce42
Elster <itari@yandex.ru>
2020-12-19 18:52:06 +0700

    refactor vars and structure

--- pkg/credit/credit.go
+++ pkg/credit/credit.go
@@ -13,15 +13,15 @@ import (
 // Calculate the credit
 func Calculate(sumCredit, periodCredit, rateCredit int64) (monthPayment, sumPayment, overPayment int64) {
 	// Ежемесячная процентная ставка
-	i := float64(rateCredit) / 12 / 100
+	percent := float64(rateCredit) / 12 / 100
 	// fmt.Printf("Ежемесячная процентная ставка: %0.4v\n", i)
 
 	// Коэфицент аннуитета
-	k := i * math.Pow((1+i), float64(periodCredit)) / (math.Pow((1+i), float64(periodCredit)) - 1)
+	rate := percent * math.Pow((1+percent), float64(periodCredit)) / (math.Pow((1+percent), float64(periodCredit)) - 1)
 	// fmt.Printf("Коэфицент аннуитета: %.4v\n", k)
 
-	a := k * float64(sumCredit)
-	monthPayment = int64(a) * 100
+	result := rate * float64(sumCredit)
+	monthPayment = int64(result) * 100
 	sumPayment = (periodCredit * monthPayment) * 100
 	overPayment = (sumPayment - sumCredit) * 100
 
